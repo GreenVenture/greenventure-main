f6211fbf849ea0360485b35e8e09fb409b9015db
HZKMaurice <zikaiho@gmail.com>
2023-03-31 19:38:01 +0800

    chore:add walletdatabase

--- load.sql
+++ load.sql
@@ -37,3 +37,40 @@ INSERT INTO `VOUCHER` (`value`,`merchant_name`,`voucher_name`,`voucher_code`,`de
 INSERT INTO `VOUCHER` (`value`,`merchant_name`,`voucher_name`,`voucher_code`,`description`,`pointsRequired`,`quantity`) VALUES(1, 'GongCha', 'GongCha $1 Voucher','2303GONGCHA1','$1 OFF total spending, minimum spending of $10 is required',1000,6700);
 INSERT INTO `VOUCHER` (`value`,`merchant_name`,`voucher_name`,`voucher_code`,`description`,`pointsRequired`,`quantity`) VALUES(2, 'Liho', 'Liho $2 Voucher','2303LIHO2','$2 OFF total spending, minimum spending of $10 is required',1500,10000);
 INSERT INTO `VOUCHER` (`value`,`merchant_name`,`voucher_name`,`voucher_code`,`description`,`pointsRequired`,`quantity`) VALUES(5, 'Redmart', 'Redmart $5 Voucher','2303REDMART5','$5 OFF total spending, minimum spending of $30 is required',1500,10000);
+
+
+/******************************************************************************/
+-- walletDB script
+/******************************************************************************/
+
+DROP DATABASE IF EXISTS walletDB;
+CREATE DATABASE IF NOT EXISTS walletDB;
+
+USE walletDB;
+
+CREATE TABLE IF NOT EXISTS `WALLET`(
+    `walletID` INT AUTO_INCREMENT,
+    `userID` VARCHAR(50),
+    `points_remaining` INT,
+    CONSTRAINT wallet_pk PRIMARY KEY (`walletID`)
+);
+
+CREATE TABLE IF NOT EXISTS `WALLETVOUCHER`(
+    `walletID` INT,
+    `voucherID` VARCHAR(20),
+    `voucher_code` VARCHAR(20),
+    `used` boolean,
+    CONSTRAINT walletvoucher_pk PRIMARY KEY (`walletID`, `voucherID`,`voucher_code`),
+    CONSTRAINT walletvoucher_fk1 FOREIGN KEY (`walletID`) REFERENCES `WALLET` (`walletID`)
+);
+
+
+-- inserting mock data
+
+INSERT INTO `WALLET` (`userID`,`points_remaining`) VALUES('162804203307686',100);
+INSERT INTO `WALLET` (`userID`,`points_remaining`) VALUES('162804203307687',50);
+INSERT INTO `WALLET` (`userID`,`points_remaining`) VALUES('162804203307689',300);
+
+INSERT INTO `WALLETVOUCHER` VALUES('00001','00001','2303FAIRPRICE5', false);
+INSERT INTO `WALLETVOUCHER` VALUES('00001','00002','2303GONGCHA1', false);
+INSERT INTO `WALLETVOUCHER` VALUES('00003','00003','2303LIHO2', false);
