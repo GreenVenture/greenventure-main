b051ce17c5fb5eb167b53bf82d3f223ef9552d2b
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2023-03-31 23:49:30 +0800

    feat: modify patch api endpoint to add points instead of replace

--- server/leaderboards/controllers/leaderboards.js
+++ server/leaderboards/controllers/leaderboards.js
@@ -46,12 +46,14 @@ leaderboardsRouter.post('/', async (request, response, next) => {
 /* This is a PATCH request to the users endpoint. It is updating a user in the database. */
 leaderboardsRouter.patch('/:userId', async (request, response, next) => {
     const { body } = request;
-
+  
     try {
-      const updatedUser = await Leaderboard.findOneAndUpdate({ userId: request.params.userId }, body, { new: true });
-      response.status(204).json(updatedUser);       
+        const userToUpdate = await Leaderboard.findOne({ userId: request.params.userId });
+        const updatedPoints = userToUpdate.points + body.points;
+        const updatedUser = await Leaderboard.findOneAndUpdate({ userId: request.params.userId }, { points: updatedPoints }, { new: true });
+        response.status(204).json(updatedUser);
     } catch (exception) {
-      next(exception);
+        next(exception);
     }
 });
 
