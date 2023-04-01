8e977699be98529f0696693ec55340ec74df8fc2
a2ron-k <aaronkwah@gmail.com>
2023-04-01 12:30:39 +0800

    fix: commentPost service

--- server/commentPost/commentPost.py
+++ server/commentPost/commentPost.py
@@ -31,46 +31,67 @@ def postComment():
 
     print("----------- [3] SEND POST TO COMMUNTIY SERVICE ------------")
     response = invoke_http(COMMUNITY_URL + f"post/{post_id}", method="POST", json=data)
+    print("----------- [4] SUCCESS - POST TO COMMUNTIY SERVICE ------------")
 
-    if response["code"] == 201:
-        print("----------- [4] SUCCESS POST TO COMMUNTIY SERVICE ------------")
+    if response["code"] in range(200, 300):
+        print("----------- [5] PREPARING TO NOTIFY USER ------------")
 
         print(
-            "----------- [5] RETRIEVE USERID FROM COMMUNITY SERVICE USING POSTID------------"
+            "----------- [6] RETRIEVE POSTER's ID FROM COMMUNITY SERVICE USING POSTID------------"
         )
-        response = invoke_http(COMMUNITY_URL + f"user/{post_id}", method="POST")
-        if response["code"] == 201:
+        response = invoke_http(COMMUNITY_URL + f"user/{post_id}", method="GET")
+        if response["code"] in range(200, 300):
             poster_id = response["data"]["posterID"]
             print(
-                "----------- [6] RETRIEVE EMAIL FROM ACCOUNT SERVICE USING USERID------------"
+                "----------- [7] RETRIEVE EMAIL FROM ACCOUNT SERVICE USING POSTER's ID------------"
             )
 
             response = invoke_http(ACCOUNT_URL + f"{poster_id}", method="GET")
             email = response[0]["data"]["email"]
 
-        print("----------- [7] POST REQUEST TO AMQP (SIMPLE SERVICE) ------------")
+            print("----------- [8] POST REQUEST TO AMQP (SIMPLE SERVICE) ------------")
 
-        def send_to_lavinmq(message):
-            channel.basic_publish(exchange="", routing_key=queue_name, body=message)
+            def send_to_lavinmq(message):
+                channel.basic_publish(exchange="", routing_key=queue_name, body=message)
 
-        url = "amqps://pjfowojn:hi7ZiPRdS6bEQDHZo-_CKeLH7vbINRCn@possum.lmq.cloudamqp.com/pjfowojn"
-        params = pika.URLParameters(url)
-        connection = pika.BlockingConnection(params)
-        channel = connection.channel()
-        queue_name = "notifications"
+            url = "amqps://pjfowojn:hi7ZiPRdS6bEQDHZo-_CKeLH7vbINRCn@possum.lmq.cloudamqp.com/pjfowojn"
+            params = pika.URLParameters(url)
+            connection = pika.BlockingConnection(params)
+            channel = connection.channel()
+            queue_name = "notifications"
 
-        my_object = {
-            "email": email,
-            "message": f"{name} has commented on your post",
-            "subject": "[Notifiation] Someone commented on your post!",
-        }
-        message = json.dumps(my_object)
-        send_to_lavinmq(message)
-        # Close the connection
-        connection.close()
+            my_object = {
+                "email": email,
+                "message": f"{name} has commented on your post",
+                "subject": "[Notifiation] Someone commented on your post!",
+            }
+            message = json.dumps(my_object)
+            send_to_lavinmq(message)
+            # Close the connection
+            connection.close()
 
-        print("----------- [8] SUCCESS - ALL SERVICES UPDATED ------------")
+            print("----------- [9] SUCCESS - ALL SERVICES UPDATED ------------")
+            return (
+                jsonify(
+                    {
+                        "code": 200,
+                        "message": "Notification Sent!",
+                    }
+                ),
+                200,
+            )
+        else:
+            print("Comment Notifcation might not be working!")
+            return (
+                jsonify(
+                    {
+                        "code": 404,
+                        "message": "Comment Notifcation might not be working!",
+                    }
+                ),
+                404,
+            )
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5208, debug=True)
+    app.run(host="0.0.0.0", port=5206, debug=True)
